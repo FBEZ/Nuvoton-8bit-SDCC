133eced738059f9f79efa84f4b4bf9a2f415f3a6
fbez <francesco.bez@gmail.com>
2023-02-16 15:38:12 +0100

    started project.mk

--- nuvoton-sdcc/make/project.mk
+++ nuvoton-sdcc/make/project.mk
@@ -1,1 +1,22 @@
 # Makefile travelling and building the components directory 
+
+.PHONY: build clean test
+
+# Default path to the project: we assume the Makefile including this file
+# is in the project directory
+ifndef PROJECT_PATH
+PROJECT_PATH := $(abspath $(dir $(firstword $(MAKEFILE_LIST))))
+export PROJECT_PATH
+endif
+
+
+# The directory where we put all objects/libraries/binaries. The project Makefile can
+# configure this if needed.
+ifndef BUILD_DIR_BASE
+BUILD_DIR_BASE := $(PROJECT_PATH)/build
+endif
+export BUILD_DIR_BASE
+
+test:
+	@echo $(PROJECT_PATH)
+	@echo $(BUILD_DIR_BASE)
